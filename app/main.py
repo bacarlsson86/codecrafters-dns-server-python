bca1cd0f1f8b90ae6bec2a4357d997b642544271
Bjorn Carlsson <bacarlsson@gmail.com>
2026-05-19 21:50:17 -0500

    CLI tests (cli-test-1779245417919)

--- app/main.py
+++ app/main.py
@@ -1,19 +1,33 @@
 import socket
 
+DNS_HEADER_FIELDS = {
+    "ID":      16,
+    "QR":       1,
+    "OPCODE":   4,
+    "AA":       1,
+    "TC":       1,
+    "RD":       1,
+    "RA":       1,
+    "Z":        3,
+    "RCODE":    4,
+    "QDCOUNT": 16,
+    "ANCOUNT": 16,
+    "NSCOUNT": 16,
+    "ARCOUNT": 16,
+}
+
+def parse(buf):
+    raw_binary = 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # TODO: Uncomment the code below to pass the first stage
-    #
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
     
     while True:
         try:
-            buf, source = udp_socket.recvfrom(512)
-    
+            buf, source = udp_socket.recvfrom(512) # buf is the raw binary, source is the address of the sender
+            print(''.join(format(byte, '08b') for bye in buf))
+            print(source)
             response = b""
     
             udp_socket.sendto(response, source)
