e1be7ebea259ede3107337a42d2d35f9c0c63df2
Bjorn Carlsson <bacarlsson@gmail.com>
2026-05-19 21:27:27 -0500

    CLI tests (cli-test-1779244047234)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import socket
 
+class DNS_Message(raw){
+
+}
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,7 +16,8 @@ def main():
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-    
+            print(buf)
+            print(source)
             response = b""
     
             udp_socket.sendto(response, source)
